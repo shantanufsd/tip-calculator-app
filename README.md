e958cf6103352be28adff03e9844f6f7d1effb2a
Shantanu <shantanupratap121@gmail.com>
2023-03-02 13:48:24 +0530

    Add Tip Calculator App files and implement functionality

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Frontend Mentor - Tip calculator app
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![Screenhsot of tip calculator app on desktop](./screenshots/tip-calculator-app-screenshot-desktop.png)
+![Screenshot of tip calculator app on mobile](./screenshots/tip-calculator-app-screenshot-mobile.png)
+
+### Links
+
+- Solution URL: [Tip Calculator App](https://github.com/shantanufsd/tip-calculator-app)
+- Live Site URL: [View](https://shantanufsd.github.io/tip-calculator-app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- SCSS
+
+### What I learned
+
+I learned how to implement SCSS in this project.
+
+## Author
+
+- Frontend Mentor - [@shantanufsd](https://www.frontendmentor.io/profile/shantanufsd)
+- Twitter - [@shantanufsd](https://www.twitter.com/shantanufsd)
--- js/app.js
+++ js/app.js
@@ -1,11 +1,150 @@
 const headerElement = document.querySelector(".header");
+const billAmount = document.querySelector("#bill-amount");
+const customTip = document.querySelector("#custom-tip");
+const totalPeoples = document.querySelector("#total-peoples");
+const tipPerPerson = document.querySelector("#tip-per-person");
+const tipTotalPerPerson = document.querySelector("#tip-total-per-person");
+const billMsg = document.querySelectorAll(".bill-msg");
+const tipButtons = document.querySelectorAll(".tip-button");
+const resetBtn = document.querySelector(".reset-btn");
+const errorMessage = document.querySelectorAll(".error-message");
 
 const pageTitle = headerElement.innerHTML;
 const PageTitleUpperCase = pageTitle.toUpperCase();
-const firstHalfOfTitle = PageTitleUpperCase.slice(0,4);
-const secondHalfOfTitle  = PageTitleUpperCase.slice(4);
+const firstHalfOfTitle = PageTitleUpperCase.slice(0, 4);
+const secondHalfOfTitle = PageTitleUpperCase.slice(4);
 const outputString = `<span class="first-half">${firstHalfOfTitle}</span><span class="second-half">${secondHalfOfTitle}</span>`;
-console.log(outputString);
-headerElement.innerHTML= outputString;
-// string.toUpperCase();
-// console.log(string);
+headerElement.innerHTML = outputString;
+
+let totalAmount;
+let tip;
+let peoples;
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+function amountPerPerson(bill, tip, peoples) {
+  if(bill && tip && peoples){
+    const tipConvert = tip / 100;
+    const totalTip = (bill * tipConvert) / peoples;
+    return bill / peoples + totalTip;
+  }
+}
+
+
+function tipAmountPerPerson(bill, tip, peoples) {
+  if(bill && tip && peoples) {
+    const tipConvert = tip / 100;
+    return (bill * tipConvert) / peoples;
+  }
+
+}
+
+function displayValue() {
+  const result = tipAmountPerPerson(totalAmount, tip, peoples);
+  if(result !== undefined && !isNaN(result)){
+    tipPerPerson.innerHTML = "$" + result.toFixed(2);
+  }
+  const result2 = amountPerPerson(totalAmount, tip, peoples);
+  if(result2 !== undefined && !isNaN(result2)){
+    tipTotalPerPerson.innerHTML = "$" + result2.toFixed(2);
+  }
+
+}
+
+function checkValid(){
+  if(totalAmount>999999){
+    totalAmount = 999999;
+  }else if(tip>100){
+    tip = 100;
+  }
+  return true;
+}
+
+function errorMessageCreate(){
+  for(let i=0; i<billMsg.length; i++ ){
+    const errorMessage = document.createElement("p");
+    errorMessage.innerHTML = "can't be zero";
+    errorMessage.classList.add("error-message");
+    billMsg[i].appendChild(errorMessage);
+  }
+}
+errorMessageCreate();
+
+function updateErrorMsg(inputElement){
+  if(inputElement.value === "" || inputElement.value === "0"){
+    inputElement.classList.add("error-alert");
+    inputElement.nextElementSibling.style.display = "block";
+  } else {
+    inputElement.classList.remove("error-alert");
+    inputElement.nextElementSibling.style.display = "none";
+  }
+}
+
+function resetValue(){
+  totalAmount = "";
+  tip = "";
+  peoples = "";
+  customTip.value = "";
+  tipPerPerson.innerHTML = "$0.00";
+  tipTotalPerPerson.innerHTML = "$0.00";
+  billAmount.value="";
+  totalPeoples.value="";
+  billAmount.classList.remove("error-alert");
+  totalPeoples.classList.remove("error-alert");
+  billAmount.nextElementSibling.style.display = "none";
+  totalPeoples.nextElementSibling.style.display = "none";
+  tipButtons.forEach(btn => btn.classList.remove('btn-click'));
+  resetBtn.disabled = true;
+}
+
+resetBtn.addEventListener("click", resetValue);
+
+billAmount.oninput = (event) => {
+  totalAmount = parseFloat(event.target.value);
+  if (checkValid()) {
+    displayValue();
+  }
+  updateErrorMsg(event.target);
+  resetBtn.disabled = false;
+};
+
+customTip.oninput = (event) => {
+  tipButtons.forEach(btn => btn.classList.remove('btn-click'));
+  tip = parseInt(event.target.value);
+  if (checkValid()) {
+    displayValue();
+  }
+  resetBtn.disabled = false;
+};
+
+customTip.addEventListener('focus', () => {
+  tipButtons.forEach(btn => btn.classList.remove('btn-click'));
+});
+
+tipButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    customTip.value = "";
+    tipButtons.forEach(btn => btn.classList.remove('btn-click'));
+    button.classList.add("btn-click");
+    tip = parseInt(button.textContent);
+    if (checkValid()) {
+      displayValue();
+    }
+    resetBtn.disabled = false;
+  });
+});
+
+
+totalPeoples.oninput = (event) => {
+  peoples = parseInt(event.target.value);
+  if (checkValid()) {
+    displayValue();
+  }
+  updateErrorMsg(event.target);
+  resetBtn.disabled = false;
+};
+
+
